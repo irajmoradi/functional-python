bfc0a9574e84dcfa7a10c16e53d0f93955c698ba
irajmoradi <irajjmoradi@gmail.com>
2023-01-26 18:26:50 -0800

    did the homework I think hopefully

--- Fixme.py
+++ Fixme.py
@@ -5,9 +5,11 @@ Your solutions should use the map and filter functions,
 and not for loops or list comprehensions.
 '''
 
+
 def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
+
 
     >>> evens(10)
     [0, 2, 4, 6, 8, 10]
@@ -20,11 +22,14 @@ def evens(n):
     >>> evens(-1)
     []
     '''
+    return list(filter(lambda x: x % 2 == 0, range(n+1)))
 
 
 def threes(n):
     '''
-    Returns a list of all numbers from 0 to n inclusive that contain the digit 3.
+    Returns a list of all numbers from 0 to n
+    inclusive that contain the digit 3.
+
 
     >>> threes(2)
     []
@@ -37,11 +42,13 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
+    return list(filter(lambda x: '3' in str(x), range(n + 1)))
 
 
 def small_words(text):
     '''
-    Returns a list of all words in the input text that are less than 4 characters long.
+    Returns a list of all words in the input
+    text that are less than 4 characters long.
 
     HINT:
     Recall that text.split() converts the text variable into a list of words.
@@ -55,6 +62,7 @@ def small_words(text):
     >>> small_words('a big word is bad')
     ['a', 'big', 'word', 'is', 'bad']
     '''
+    return list(filter(lambda x: len(x) < 5, text.split()))
 
 
 def squares(n):
@@ -71,11 +79,13 @@ def squares(n):
     >>> squares(10)
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
+    return list(map(lambda x: x**2, range(1, n + 1)))
 
 
 def lengths(strings):
     '''
-    Given a list of strings, returns a list of the lengths of the corresponding strings.
+    Given a list of strings,
+    returns a list of the lengths of the corresponding strings.
 
     >>> lengths([])
     []
@@ -84,3 +94,4 @@ def lengths(strings):
     >>> lengths(['this','is','a','test'])
     [4, 2, 1, 4]
     '''
+    return list(map(lambda x: len(x), strings))
